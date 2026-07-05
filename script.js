9f0ef4f39dd1fd87bc4fe6ad8b98f7cdf3522386
0558xw-bit <05.58.xw@gmail.com>
2026-07-05 17:42:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,53 +8,63 @@ function sum(arr) {
 }
 
 function generate_sp(amount, max) {
-    amount -= 1;
-
-    // ----------------
-
-    function gen(max) {
-        return Math.floor(Math.random()*100*max)/100;
-    }
 
     // ----------------
     // Initialize
 
-    var point = [];
-    point.length = amount;
-
-    var sp = [];
-    sp.length = point.length + 1;
+    let point = new Array(amount + 1).fill(null);
+    let sp = new Array(amount).fill(null);
 
     // -----------------
-    // generate
+    // generate points
 
-    for(let i = 0; i < point.length; i++) {
-        point[i] = gen(max);
+    point[0] = 0;
+    point[point.length-1] = max;
+
+    for(let i = 1; i < point.length - 1; i++) {
+        point[i] = Math.floor(Math.random()*100*max)/100;
     }
 
-    // sort
+    // sort points
     point = point.sort(function (a, b) {  return a - b;  });
 
-    // find sp
-    sp[0] = point[0] - 0;                           // sp [0]
-    sp[sp.length-1] = max - point[point.length-1];  // sp [-1]
+    // calculate sp    
+    for(let i = 0; i < sp.length; i++) sp[i] = point[i+1]-point[i];
 
-    for(let i = 1; i < sp.length-1; i++) {          // sp [i]
-        sp[i] = point[i]-point[i-1];
-    }
-
-    // round
+    // round sp
     for(let i = 0; i < sp.length; i++) sp[i] = +sp[i].toFixed(2);    
 
     // ------------------
     return sp;
 }
 
-const newsp = [generate_sp(5, 100), generate_sp(3, 30)];
+function generate_balanced_sp() {
+    let sp = generate_sp(5, 100-15*5);
 
-function format_sp() {
+    for(let i = 0; i < 5; i++) {
+        sp[i] += 15;
+    }
 
-    const spnames = [
+    return sp;
+}
+
+function generate_leaned_sp(group, lean) {
+    const groups = [[3, 4], [1, 2], [0]];
+    const leans = [20, 35];
+
+    let sp = generate_sp(5, 100-leans[lean]*groups[group].length);
+    const lean_sp = generate_sp(groups[group].length, leans[lean]*groups[group].length);
+
+    for(let i = 0; i < groups[group].length; i++) {
+        sp[groups[group][i]] += lean_sp[i];
+    }
+    
+    return sp;
+}
+
+function format_sp(sp) {
+
+    const sp_names = [
         [
             "Сила удара",
             "Количество энергии",
@@ -69,23 +79,75 @@ function format_sp() {
         ]
     ];
 
-    let spheaders = [ "Первичные боевые параметры", "Вторичные боевые параметры" ];
+    let sp_headers = [ "Первичные боевые параметры", "Вторичные боевые параметры" ];
 
-    let sptable = "";
+    let sp_table = "";
 
     let pm = "";
 
-    for(let group = 0; group < newsp.length; group++) {
-        sptable += "<tr><th colspan='2'><b>" + spheaders[group] +"</b><th></tr>";
-        for(let i = 0; i < newsp[group].length; i++) {
+    for(let group = 0; group < sp.length; group++) {
+        sp_table += "<tr><th colspan='2'><b>" + sp_headers[group] +"</b><th></tr>";
+        for(let i = 0; i < sp[group].length; i++) {
 
             if((group == 1) && (i === 0 || i === 1)) pm = "-";
             else pm = "+";
 
-            sptable += "<tr><td>"+ pm + newsp[group][i].toFixed(1) + "%</td><td>" + spnames[group][i] + "</td></tr>";
+            sp_table += "<tr><td>"+ pm + sp[group][i].toFixed(1) + "%</td><td>" + sp_names[group][i] + "</td></tr>";
         } 
-        sptable += "<tr><td colspan='2' class='sum'>сумма: " +sum(newsp[group]).toFixed(1) +"%<td></tr>";
+        sp_table += "<tr><td colspan='2' class='sum'>сумма: " +sum(sp[group]).toFixed(1) +"%<td></tr>";
     }
 
-    document.getElementById("sp").innerHTML = sptable;
-} format_sp();
+    document.getElementById("sp").innerHTML = sp_table;
+}
+
+const new_sp = [generate_sp(5, 100), generate_sp(3, 30)];
+format_sp(new_sp);
+
+
+var form = document.getElementById("select_group");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    var data = new FormData(form);
+    var value = data.get('type');
+    
+    console.log(value);
+    switch(value) {
+        case "1":
+            format_sp([generate_sp(5, 100), generate_sp(3, 30)]);
+            break;
+        case "2":
+            format_sp([generate_balanced_sp(), generate_sp(3, 30)]);
+            break;
+        case "3":
+            format_sp([generate_leaned_sp(0, 0), generate_sp(3, 30)]);
+            break;
+        case "4":
+            format_sp([generate_leaned_sp(0, 1), generate_sp(3, 30)]);
+            break;
+        case "5":
+            format_sp([generate_leaned_sp(1, 0), generate_sp(3, 30)]);
+            break;
+        case "6":
+            format_sp([generate_leaned_sp(1, 1), generate_sp(3, 30)]);
+            break;
+        case "7":
+            format_sp([generate_leaned_sp(2, 0), generate_sp(3, 30)]);
+            break;
+        case "8":
+            format_sp([generate_leaned_sp(2, 1), generate_sp(3, 30)]);
+            break;            
+    }
+});
+
+  
+
+
+
+
+
+
+ 
+
+
